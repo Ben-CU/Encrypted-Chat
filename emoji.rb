a28f56a644e22e61561aa2e2372bfc89e88801d3
Owen Smith <owensmith0031@gmail.com>
2015-11-05 23:21:30 +0000

    Update emoji.rb

--- emoji.rb
+++ emoji.rb
@@ -1,7 +1,29 @@
 def emoji (m)
-  f = IO.readlines("econtent.txt")
-  m.sub! '/bigmona', f[0,70].to_s
-  m.sub! '/dickbutt', f[76,149].to_s
+  def read_file(file_name)
+    file = File.open(file_name, "r")
+    data = file.read
+    file.close
+    return data
+  end
+  if m.include? "/bigmona"
+    mona = read_file("asciiart/mona.txt")
+  end
+
+  if m.include? "/dickbutt"
+    dickbutt = read_file("asciiart/dickbutt.txt")
+  end
+
+  if m.include? "/happy"
+    happy = read_file("asciiart/happy.txt")
+  end
+
+
+
+
+  m.sub! '/bigmona', mona
+  m.sub! '/dickbutt', dickbutt
+  m.sub! '/happy', ':D'
+  m.sub! '/etower', "h"
   return m
 end
 
@@ -9,18 +31,13 @@ end
 
 
 
-
-# puts emoji("/dickbutt")
-
+ puts emoji("/bigmona /dickbutt")
 
 
 
 
 
 
-
-
-#ignore
 def read_file(file_name)
   file = File.open(file_name, "r")
   data = file.read
